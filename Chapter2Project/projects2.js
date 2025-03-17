8af8612f64f59c7574fb3616b002b100b8ba9c74
dalvijccc <dalvi@stumail.jccc.edu>
2025-03-17 16:48:34 -0500

    Create projects2.js

---
+++ Chapter2Project/projects2.js
@@ -0,0 +1,23 @@
+let myName = "Danyal Alvi";
+
+let para1 = document.getElementById("p1");
+para1.textContent = myName;
+
+let n1 = 8;
+let n2 = 5;
+
+let numberSum = n1 + n2;
+document.getElementById("p2").textContent = numberSum;
+
+let numberMult = n1 * n2;
+document.getElementById("p3").textContent = numberMult;
+
+let myNameAddNum = myName + n1;
+document.getElementById("p4").textContent = myNameAddNum;
+
+let myNameMultNum = myName * n1;
+document.getElementById("p5").textContent = myNameMultNum;
+
+let myAge = 27;
+let ageCompare = myAge > numberMult;
+document.getElementById("p6").textContent = ageCompare;

0545bac42399d22b7525c46a7d4cbc5e0a7283f8
dalvijccc <dalvi@stumail.jccc.edu>
2025-04-21 21:14:51 -0500

    Add files via upload

---
+++ Chapter3Project/script.js
@@ -0,0 +1,30 @@
+// Author: Danyal Alvi
+// Date: April 21, 2025
+
+// 1. Full name
+let myName = "Danyal Alvi";
+console.log("My name:", myName);
+
+// 2. Desired annual salary
+let mySalary = 85000;
+console.log("My desired annual salary:", mySalary);
+
+// 3. Veteran status
+let isVeteran = false
+console.log("Am I a veteran?", isVeteran);
+
+// 4. Array of three friends' names
+let friendsNames = ["Ralph", "Jesse", "Jameson"];
+console.log("My friends are", friendsNames);
+
+// 5. Array of their desired salaries
+let friendsSalaries = [70000, 65000, 90000];
+console.log("My friends' desired salaries are", friendsSalaries);
+
+// 6. Object for another friend's info
+let anotherFriend = {
+  firstName: "Jacob",
+  lastName: "Bell",
+  desiredSalary: 75000
+};
+console.log("Another friend's info:", anotherFriend);
